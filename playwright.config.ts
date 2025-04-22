16e160aa9fd602ab41be0f1e316a54ed213ebd6e
piotr-spy <piotrek.spychala@gmail.com>
2025-04-22 16:36:39 +0200

    DA-2 Added mobile executions to config

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'Desktop - Chrome',
       use: { ...devices['Desktop Chrome'] },
     },
 
@@ -45,19 +45,19 @@ export default defineConfig({
     // },
 
     {
-      name: 'webkit',
+      name: 'Desktop - Safari',
       use: { ...devices['Desktop Safari'] },
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile - Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile - Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
