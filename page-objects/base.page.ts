5902ff0a9fa9759ece7a2db7235e656db1de820e
piotr-spy <piotrek.spychala@gmail.com>
2025-04-22 15:53:10 +0200

    DA-2 Added base page object

---
+++ page-objects/base.page.ts
@@ -0,0 +1,21 @@
+import { Page } from "@playwright/test";
+
+/**
+ * Base Page is a page object containing all methods, selectors, and functionalities
+ * shared across all page objects
+ */
+export class BasePage {
+    protected page: Page;
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    public async getTitle(): Promise<string> {
+        return await this.page.title()
+    }
+    
+    public async open(url: string): Promise<void> {
+        await this.page.goto(``);
+    }
+}
