13193213dea230f2dd001f20296b7fd305da27b2
piotr-spy <piotrek.spychala@gmail.com>
2025-04-24 11:27:08 +0200

    DA-3 Added basic post test

--- e2e/api/fakeStore.spec.ts
+++ e2e/api/fakeStore.spec.ts
@@ -1,11 +1,15 @@
 import { expect, test } from '@playwright/test';
+import { newProduct } from '../../test-data/products';
+
+const products = {
+    new: newProduct
+}
 
 test.describe('Fake Store API Tests', () => {
 
     test('Get all products and validate response', async ({ request }) => {
         const response = await request.get('https://fakestoreapi.com/products')
         const products = await response.json()
-        
         await expect(response).toBeOK()
         expect(response.status()).toBe(200)
         products.forEach(product => {
@@ -22,5 +26,18 @@ test.describe('Fake Store API Tests', () => {
             expect(product.rating.rate).toBeLessThanOrEqual(5)
             expect(product.rating.count).toBeGreaterThanOrEqual(0)
         })
+    })
+
+    test('Create new product and validate response @postTest', async ({ request }) => {
+        const response = await request.post('https://fakestoreapi.com/products', {
+            data: products.new
+        })
+        const product = await response.json()
+        const { id, ...responseProductWithoutID } = product
+        await expect(response).toBeOK()
+        expect(response.status()).toBe(200)
+        expect(product).toHaveProperty('id')
+        expect(product.id).toBeGreaterThan(0)
+        expect(responseProductWithoutID).toEqual(products.new)
     })
 })
