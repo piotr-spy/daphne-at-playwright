69977bcb86558688d37ea3f9d5ac96e9d3b5a233
piotr-spy <piotrek.spychala@gmail.com>
2025-04-24 11:18:42 +0200

    DA-3 Added new product for post method to test data

---
+++ test-data/products.ts
@@ -0,0 +1,8 @@
+
+export const newProduct = {
+        "title": "New Product",
+        "price": 1199.99,
+        "description": "Your new awesome product is here!",
+        "category": "Audio Devices",
+        "image": ""
+}
