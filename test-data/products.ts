ae3a0b13e0a46903ab5c703c15c2107e92067a2b
piotr-spy <piotrek.spychala@gmail.com>
2025-04-30 14:29:39 +0200

    DA-3 Added updated product data

--- test-data/products.ts
+++ test-data/products.ts
@@ -6,3 +6,16 @@ export const newProduct = {
         "category": "Audio Devices",
         "image": ""
 }
+
+export const updatedProduct = {
+        "id": 8,
+        "title": "Pierced Owl Rose Gold Plated Stainless Steel Double",
+        "price": 15.99,
+        "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+        "category": "jewelery",
+        "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+        "rating": {
+            "rate": 1.9,
+            "count": 100
+        }
+    }
