78635e0a09ca6f9b706b55dc1e26a800e98be73a
piotr-spy <piotrek.spychala@gmail.com>
2025-05-05 14:35:38 +0200

    DA-3 added deleted product data

--- test-data/products.ts
+++ test-data/products.ts
@@ -15,3 +15,16 @@ export const updatedProduct = {
         "category": "jewelery",
         "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
     }
+
+export const deletedProduct = {
+        "id": 9,
+        "title": "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+        "price": 64,
+        "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+        "category": "electronics",
+        "image": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+        "rating": {
+            "rate": 3.3,
+            "count": 203
+        }
+    }
