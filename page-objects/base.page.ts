ed099acb69e69b0af048f2ee022ebaa1c9bd2660
Piotr Spychała <piotr.spychala@strix.net>
2026-06-02 15:10:53 +0200

    DA-3 Updated web tests

--- page-objects/base.page.ts
+++ page-objects/base.page.ts
@@ -5,7 +5,7 @@ import { Page } from "@playwright/test";
  * shared across all page objects
  */
 export class BasePage {
-    protected page: Page;
+    protected readonly page: Page;
 
     constructor(page: Page) {
         this.page = page
